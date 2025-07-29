119569c237970a96c653aa7acf83a5a917d8900e
ph <pedro.xit.fluxo.ff@gmail.com>
2025-07-29 09:12:57 -0300

    Funções restantes 4 e 5

--- base.js
+++ base.js
@@ -145,4 +145,33 @@ function coletarDadosPartida(torneio) {
   });
 }
 
+function removerTorneio() {
+  if (torneios.length === 0) {
+      console.log('Nenhum torneio registrado para remover.');
+      console.log('\nPressione Enter para retornar ao menu...');
+      return rl.question('', menu);
+  }
+
+  console.log('\n=== TORNEIOS DISPONÍVEIS ===');
+  torneios.forEach((torneio, index) => {
+      console.log(`${index + 1}. ID: ${torneio.id} | Nome: ${torneio.nome} | Jogo: ${torneio.jogo} | Data: ${torneio.data}`);
+  });
+
+  rl.question('\nDigite o ID do torneio que deseja apagar: ', (idParaRemover) => {
+
+      const idNumerico = parseInt(idParaRemover, 10);
+
+      const index = torneios.findIndex(torneio => torneio.id == idNumerico);
+      if (index !== -1) {
+          const [removido] = torneios.splice(index, 1);
+          console.log(`Torneio "${removido.nome}" (ID: ${removido.id}) foi removido com sucesso!`);
+      } else {
+          console.log('ID do torneio inválido ou não encontrado!');
+      }
+
+      console.log('\nPressione Enter para voltar ao menu...');
+      rl.question('', menu);
+  });
+}
+
 menu();
